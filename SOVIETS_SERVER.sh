46399ca300393a5f1c1d944c13ef2b92b9b36d63
dj005112 <david.o'donnell@digitalglobe.com>
2019-03-15 15:01:47 -0600

    made bash scripts

---
+++ SOVIETS_SERVER.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+source ~/.bashrc
+
+# Read in non-mod args
+args=""
+while read LINE;
+    #do echo "$LINE"
+    do args="$args $LINE"
+done < server_args.txt
+
+args="$args mod=\""
+while read LINE;
+    do args="$args$LINE"
+done < base_mods.txt
+while read LINE;
+    do args="$args$LINE"
+done < soviets_mods.txt
+args="$args\""
+
+exe=C:/Program\ Files\ \(x86\)/Steam/steamapps/common/Arma\ 3\ Server/arma3server_x64.exe
+
+echo "Starting Server using ${exe}${args}"
+
+${exe}${args}
