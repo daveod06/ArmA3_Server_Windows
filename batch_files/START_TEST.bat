de8188257e486c80861025cb2b0ef8fcd0eaf0db
daveod06 <30670921+daveod06@users.noreply.github.com>
2019-03-28 12:09:30 -0600

    got working batch script!

--- batch_files/START_TEST.bat
+++ batch_files/START_TEST.bat
@@ -1,34 +1,46 @@
 @echo off
-//setlocal EnableExtensions EnableDelayedExpansion
-//SETLOCAL DisableDelayedExpansion
 C:\Windows\System32\tasklist /FI "IMAGENAME eq arma3server_x64.exe" 2>NUL | C:\Windows\System32\find /I /N "arma3server_x64.exe">NUL
 echo Server monitored is not running, will be started now
 
-set BaseModsFile=D:\Games\Steam\base_mods.txt
+// FILE PATHS
+set BaseModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\base_mods.txt
+set CollectionModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\soviets_mods.txt
+set ServerArgsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\server_args.txt
+set ServerExe="C:\Games\Steam\steamapps\common\Arma 3 Server\arma3server_x64.exe"
 
+// BASE MODS
+SET mods_arg="mods=
+SETLOCAL EnableDelayedExpansion
+FOR /f "delims=" %%x IN (%BaseModsFile%) DO (
+	CALL SET mods_arg=%%mods_arg%%%%x
+)
 
+// COLLECTION MODS
+FOR /f "delims=" %%x IN (%CollectionModsFile%) DO (
+	CALL SET mods_arg=%%mods_arg%%%%x
+)
+CALL SET mods_arg=%%mods_arg%%"
 
-SET base_mods=mods="
+echo mods:
+echo !mods_arg!
 
-FOR /F "usebackq delims=" %%a in (`"findstr /n ^^ %BaseModsFile%"`) do (
-    set "var=%%a"
-    SETLOCAL EnableDelayedExpansion
-    set "var=!var:*:=!"
-    SET "base_mods=%base_mods%!var!"
+// SERVER ARGS
+SET server_args=
+FOR /f "delims=" %%x IN (%ServerArgsFile%) DO (
+	CALL SET server_args=%%server_args%% %%x
 )
 
 
+echo server args:
+echo !server_args!
 
+SET full_command=start "" /wait !ServerExe! !server_args! !mods_arg!
+echo Starting ArmA 3 Server using:
+echo !full_command!
 
+start "" /wait !ServerExe! !server_args! !mods_arg!
 
-
-
-
-
-echo "base_mods:"
-echo %base_mods%
 pause
 
 
-echo Server started succesfully
-
+echo Server started succesfully!
