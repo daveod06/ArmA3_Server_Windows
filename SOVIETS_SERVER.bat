e5002413620b3fd9ec5cd5c180a7bbcf5282a116
dj005112 <david.o'donnell@digitalglobe.com>
2019-03-28 12:28:47 -0600

    batch scripts ready to test - need to fix paths

--- SOVIETS_SERVER.bat
+++ SOVIETS_SERVER.bat
@@ -1,12 +1,13 @@
+
 @echo off
 C:\Windows\System32\tasklist /FI "IMAGENAME eq arma3server_x64.exe" 2>NUL | C:\Windows\System32\find /I /N "arma3server_x64.exe">NUL
 echo Server monitored is not running, will be started now
 
 // FILE PATHS
-set BaseModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\base_mods.txt
-set CollectionModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\soviets_mods.txt
-set ServerArgsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\server_args.txt
-set ServerExe="C:\Games\Steam\steamapps\common\Arma 3 Server\arma3server_x64.exe"
+SET BaseModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\base_mods.txt
+SET CollectionModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\soviets_mods.txt
+SET ServerArgsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\server_args.txt
+SET ServerExe="C:\Games\Steam\steamapps\common\Arma 3 Server\arma3server_x64.exe"
 
 // BASE MODS
 SET mods_arg="mods=
@@ -21,8 +22,8 @@ FOR /f "delims=" %%x IN (%CollectionModsFile%) DO (
 )
 CALL SET mods_arg=%%mods_arg%%"
 
-echo mods:
-echo !mods_arg!
+//ECHO mods:
+//ECHO !mods_arg!
 
 // SERVER ARGS
 SET server_args=
@@ -31,16 +32,16 @@ FOR /f "delims=" %%x IN (%ServerArgsFile%) DO (
 )
 
 
-echo server args:
-echo !server_args!
+//ECHO server args:
+//ECHO !server_args!
 
 SET full_command=start "" /wait !ServerExe! !server_args! !mods_arg!
-echo Starting ArmA 3 Server using:
-echo !full_command!
+ECHO Starting ArmA 3 Server using:
+ECHO !full_command!
 
-start "" /wait !ServerExe! !server_args! !mods_arg!
+START "" /wait !ServerExe! !server_args! !mods_arg!
 
-pause
+PAUSE
 
 
-echo Server started succesfully!
+ECHO Server started succesfully!
---
+++ VIETNAM_HC2.bat
@@ -0,0 +1,47 @@
+
+@echo off
+C:\Windows\System32\tasklist /FI "IMAGENAME eq arma3server_x64.exe" 2>NUL | C:\Windows\System32\find /I /N "arma3server_x64.exe">NUL
+echo Server monitored is not running, will be started now
+
+// FILE PATHS
+SET BaseModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\base_mods.txt
+SET CollectionModsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\vietnam_mods.txt
+SET ServerArgsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\headless_args2.txt
+SET ServerExe="C:\Games\Steam\steamapps\common\Arma 3 Server\arma3server_x64.exe"
+
+// BASE MODS
+SET mods_arg="mods=
+SETLOCAL EnableDelayedExpansion
+FOR /f "delims=" %%x IN (%BaseModsFile%) DO (
+	CALL SET mods_arg=%%mods_arg%%%%x
+)
+
+// COLLECTION MODS
+FOR /f "delims=" %%x IN (%CollectionModsFile%) DO (
+	CALL SET mods_arg=%%mods_arg%%%%x
+)
+CALL SET mods_arg=%%mods_arg%%"
+
+//ECHO mods:
+//ECHO !mods_arg!
+
+// SERVER ARGS
+SET server_args=
+FOR /f "delims=" %%x IN (%ServerArgsFile%) DO (
+	CALL SET server_args=%%server_args%% %%x
+)
+
+
+//ECHO server args:
+//ECHO !server_args!
+
+SET full_command=start "" /wait !ServerExe! !server_args! !mods_arg!
+ECHO Starting ArmA 3 Server using:
+ECHO !full_command!
+
+START "" /wait !ServerExe! !server_args! !mods_arg!
+
+PAUSE
+
+
+ECHO Server started succesfully!
