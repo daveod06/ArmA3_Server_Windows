46e04fa71f8f7ac2508b3030d800b238677ebc48
daveod06 <davidodonnell88@gmail.com>
2019-03-31 13:26:54 -0600

    fixed mod arg by removing long file paths

--- SOVIETS_SERVER.bat
+++ SOVIETS_SERVER.bat
@@ -10,7 +10,7 @@ SET ServerArgsFile=\\infc2_file\fileserv_data01\users\dj005112\Documents\server\
 SET ServerExe="C:\Games\Steam\steamapps\common\Arma 3 Server\arma3server_x64.exe"
 
 :: BASE MODS
-SET mods_arg="mods=
+SET mods_arg="mod=
 SETLOCAL EnableDelayedExpansion
 FOR /f "delims=" %%x IN (%BaseModsFile%) DO (
 	CALL SET mods_arg=%%mods_arg%%%%x
@@ -22,24 +22,15 @@ FOR /f "delims=" %%x IN (%CollectionModsFile%) DO (
 )
 CALL SET mods_arg=%%mods_arg%%"
 
-::ECHO mods:
-::ECHO !mods_arg!
-
 :: SERVER ARGS
 SET server_args=
 FOR /f "delims=" %%x IN (%ServerArgsFile%) DO (
 	CALL SET server_args=%%server_args%% %%x
 )
 
-
-::ECHO server args:
-::ECHO !server_args!
-
-SET full_command=start "" /wait !ServerExe! !server_args! !mods_arg!
 ECHO Starting ArmA 3 Server using:
-ECHO !full_command!
-
-START "" /wait !ServerExe! !server_args! !mods_arg!
+ECHO START "ArmA3" /wait !ServerExe! !server_args! !mods_arg!
+START "ArmA3" /wait !ServerExe! !server_args! !mods_arg!
 
 PAUSE
 
--- VIETNAM_SERVER.bat
+++ VIETNAM_SERVER.bat
@@ -4,8 +4,8 @@ C:\Windows\System32\tasklist /FI "IMAGENAME eq arma3server_x64.exe" 2>NUL | C:\W
 ECHO Server monitored is not running, will be started now
 
 :: FILE PATHS
-SET BaseModsFile=base_test.txt
-SET CollectionModsFile=collection_test.txt
+SET BaseModsFile=base_mods.txt
+SET CollectionModsFile=vietnam_mods.txt
 SET ServerArgsFile=server_args.txt
 SET ServerExe="D:\Steam\steamapps\common\Arma 3 Server\arma3server_x64.exe"
 
@@ -31,8 +31,6 @@ FOR /f "delims=" %%x IN (%ServerArgsFile%) DO (
 ECHO Starting ArmA 3 Server using:
 ECHO START "ArmA3" /wait !ServerExe! !server_args! !mods_arg!
 START "ArmA3" /wait !ServerExe! !server_args! !mods_arg!
-:: START "ArmA3" /wait !ServerExe! -port=2305 -showScriptErrors -filePatching -world=empty -nosplash -skipIntro -loadMissionToMemory -name=server -bandwidthAlg=2 "-serverMod=" "-profiles=D:\Steam\steamapps\common\Arma 3 Server\Profiles" "-config=D:\Steam\steamapps\common\Arma 3 Server\config.cfg" "-cfg=D:\Steam\steamapps\common\Arma 3 Server\basic.cfg" "mod=C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@ALiVE;C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@CBA_A3;C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@CUP Terrains - Core;C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@CUP Terrains - Maps;C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@Enhanced Movement;C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@MCC Sandbox 4 -  Mission Making The Easy Way;C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@Project OPFOR;C:\Games\Steam\steamapps\common\Arma 3\!Workshop\@RHSAFRF;"
-:: START "ArmA3" /wait !ServerExe! "-par=server_args_test.txt"
 
 PAUSE
 
